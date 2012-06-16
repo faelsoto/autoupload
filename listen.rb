7366ae00ad25d896ecc3cc0df1552badaecde01e
Rafael Soto <fael.soto@gmail.com>
2012-06-16 17:55:34 -0500

    Primera prueba de upload!

--- listen.rb
+++ listen.rb
@@ -3,9 +3,14 @@ require_relative 'config'
 require_relative 'imgur'
 
 config = LocalConfig.new
+Imgur.api_key = config.api_key
+
 
 Listen.to(config.dir) do |mod, add, rm|
   unless add.nil?
     puts add
+    response = Imgur.upload_file(add.first)
+    img_url = Imgur.url_for(response[:hash], response)
+    puts img_url
   end
 end
