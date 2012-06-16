8ccd39d01791d60ffa3b95f016cb1603abc6c6aa
Rafael Soto <fael.soto@gmail.com>
2012-06-16 18:42:58 -0500

    Se agrega Guard para enviar notificaciones

--- listen.rb
+++ listen.rb
@@ -1,10 +1,16 @@
 require 'listen'
+require 'guard'
 require_relative 'config'
 require_relative 'imgur'
 
 config = LocalConfig.new
 Imgur.api_key = config.api_key
 
+if config.api_key.empty?
+  Guard::Notifier::NotifySend::notify "failure", "Autoupload", "Error: Te falta agregar la API key.", nil
+else
+  Guard::Notifier::NotifySend::notify nil, "Autoupload", "Saca screenshots!", nil
+end
 
 Listen.to(config.dir) do |mod, add, rm|
   unless add.nil?
@@ -13,5 +19,6 @@ Listen.to(config.dir) do |mod, add, rm|
     img_url = Imgur.url_for(response[:hash], response)
     puts img_url
     system("echo \"#{img_url}\" | xclip -selection clipboard")
+    Guard::Notifier::NotifySend::notify nil, "Autoupload", "URL copiada al portapapeles", nil
   end
 end
---
+++ start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ruby ~/www/autoimgur/listen.rb
