da11bb545d3ec85d21c8249a55313f9817b00901
Rafael Soto <fael.soto@gmail.com>
2012-06-16 21:43:53 -0500

    Fix de Notify, además también avisa cuando está subiendo imagen

--- listen.rb
+++ listen.rb
@@ -10,16 +10,18 @@ if config.api_key.empty?
   Guard::Notifier::NotifySend::notify "failure", "Autoupload", "Error: Te falta agregar la API key.", nil
   abort 
 else
-  Guard::Notifier::NotifySend::notify nil, "Autoupload", "Saca screenshots!", nil
+  Guard::Notifier::NotifySend::notify 'notify', "Autoupload", "Saca screenshots!", nil
 end
 
 Listen.to("#{File.dirname __FILE__}/#{config.dir}") do |mod, add, rm|
-  unless add.nil?
+  unless (add = add.first).nil?
+    Guard::Notifier::NotifySend::notify 'notify', "Autoupload", "Subiendo imagen...", nil
     puts add
-    response = Imgur.upload_file(add.first)
+    response = Imgur.upload_file(add)
+    sleep 1
     img_url = Imgur.url_for(response[:hash], response)
     puts img_url
-    system("echo \"#{img_url}\" | xclip -selection clipboard")
-    Guard::Notifier::NotifySend::notify nil, "Autoupload", "URL copiada al portapapeles", nil
+    system("echo '#{img_url}' | xclip -selection clipboard")
+    Guard::Notifier::NotifySend::notify 'notify', "Autoupload", "URL copiada al portapapeles", nil
   end
 end
